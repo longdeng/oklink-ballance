e58088aeb9dc3067a1aa684bce212cd6a2516dca
zld <longdeng0000@163.com>
2014-11-17 14:11:48 +0800

    ballance ==> balance

--- src/main/java/com/oklink/dao/bean/AppBalance.java
+++ src/main/java/com/oklink/dao/bean/AppBalance.java
@@ -12,7 +12,7 @@ import com.oklink.util.StringUtil;
  * @version 创建时间：2014-11-5 下午12:03:13
  * 类说明
  */
-public class AppBallance implements Serializable {
+public class AppBalance implements Serializable {
 	private static final long serialVersionUID = -8761333341983145683L;
 
 	private long id;
@@ -21,9 +21,9 @@ public class AppBallance implements Serializable {
 	private int code;
 	private String objectId;	//第三方平台的钱包标识（用于余额监控）
 	private String objectName;	//第三方平台的钱包名称（用于页面显示）
-	private double latestBtcBallance;
-	private double latestLtcBallance;
-	private List<AppBallance> appBallanceList;
+	private double latestBtcBalance;
+	private double latestLtcBalance;
+	private List<AppBalance> appBallanceList;
 	
 	public long getId() {
 		return id;
@@ -61,26 +61,26 @@ public class AppBallance implements Serializable {
 	public void setObjectName(String objectName) {
 		this.objectName = objectName;
 	}
-	public double getLatestBtcBallance() {
-		return latestBtcBallance;
+	public double getLatestBtcBalance() {
+		return latestBtcBalance;
 	}
-	public void setLatestBtcBallance(double latestBtcBallance) {
-		this.latestBtcBallance = latestBtcBallance;
+	public void setLatestBtcBalance(double latestBtcBalance) {
+		this.latestBtcBalance = latestBtcBalance;
 	}
-	public double getLatestLtcBallance() {
-		return latestLtcBallance;
+	public double getLatestLtcBalance() {
+		return latestLtcBalance;
 	}
-	public void setLatestLtcBallance(double latestLtcBallance) {
-		this.latestLtcBallance = latestLtcBallance;
+	public void setLatestLtcBalance(double latestLtcBalance) {
+		this.latestLtcBalance = latestLtcBalance;
 	}
-	public List<AppBallance> getAppBallanceList() {
+	public List<AppBalance> getAppBalanceList() {
 		return appBallanceList;
 	}
-	public void setAppBallanceList(List<AppBallance> appBallanceList) {
+	public void setAppBalanceList(List<AppBalance> appBallanceList) {
 		this.appBallanceList = appBallanceList;
 	}
 	
-	public static AppBallance convert(Map<String, Object> map) {
+	public static AppBalance convert(Map<String, Object> map) {
 		if(map == null){
 			return null;
 		}
@@ -91,16 +91,16 @@ public class AppBallance implements Serializable {
 				return null;
 			}
 		}
-		AppBallance appBallance = new AppBallance();
-		appBallance.setId(id);
-		if(map.get("parent_id")!=null) appBallance.setParentId(StringUtil.toLong(map.get("parent_id").toString(), 0l));
-		if(map.get("user_id")!=null) appBallance.setUserId(StringUtil.toLong(map.get("user_id").toString(), 0l));
-		if(map.get("code")!=null) appBallance.setCode(StringUtil.toInteger(map.get("code").toString(), 0));
-		if(map.get("object_id")!=null) appBallance.setObjectId(map.get("object_id").toString());
-		if(map.get("object_name")!=null) appBallance.setObjectName(map.get("object_name").toString());
-		if(map.get("latest_btc_ballance")!=null) appBallance.setLatestBtcBallance(StringUtil.toDouble(map.get("latest_btc_ballance").toString(),0d));
-		if(map.get("latest_ltc_ballance")!=null) appBallance.setLatestLtcBallance(StringUtil.toDouble(map.get("latest_ltc_ballance").toString(),0d));
-		return appBallance;
+		AppBalance appBalance = new AppBalance();
+		appBalance.setId(id);
+		if(map.get("parent_id")!=null) appBalance.setParentId(StringUtil.toLong(map.get("parent_id").toString(), 0l));
+		if(map.get("user_id")!=null) appBalance.setUserId(StringUtil.toLong(map.get("user_id").toString(), 0l));
+		if(map.get("code")!=null) appBalance.setCode(StringUtil.toInteger(map.get("code").toString(), 0));
+		if(map.get("object_id")!=null) appBalance.setObjectId(map.get("object_id").toString());
+		if(map.get("object_name")!=null) appBalance.setObjectName(map.get("object_name").toString());
+		if(map.get("latest_btc_balance")!=null) appBalance.setLatestBtcBalance(StringUtil.toDouble(map.get("latest_btc_balance").toString(),0d));
+		if(map.get("latest_ltc_balance")!=null) appBalance.setLatestLtcBalance(StringUtil.toDouble(map.get("latest_ltc_balance").toString(),0d));
+		return appBalance;
 	}
 	
 	public String toString() {
